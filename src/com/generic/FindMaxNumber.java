7c0889ca9158694b76ffab534df4c7e48660531b
SandeepSinghGaur <sandeepgaurdec13@gmail.com>
2021-01-13 04:56:58 +0530

    Refactor:Create Generic Class and find max

--- src/com/generic/FindMaxNumber.java
+++ src/com/generic/FindMaxNumber.java
@@ -1,18 +1,35 @@
 package com.generic;
 
-import java.util.ArrayList;
-
 public class FindMaxNumber {
-	public static<E extends Comparable<E>> E maxNumber(ArrayList<?> number)
-	{
-	  E max=(E) number.get(0);
-		for(Object num:number)
-		{
-			if(((Comparable<E>) num).compareTo(max)>0)
-				max=(E) num;
+
+	static class MaximumNumber<T extends Comparable<T>> {
+		T num1, num2, num3;
+
+		public MaximumNumber(T num1, T num2, T num3) {
+			this.num1 = num1;
+			this.num2 = num2;
+			this.num3 = num3;
 		}
-		System.out.println(max);
-		return max;
+
+		public T maximum() {
+			return MaximumNumber.maximum(num1, num2, num3);
+		}
+
+		public static <T extends Comparable<T>> T maximum(T num1, T num2, T num3) {
+			T max = num1;
+			if (num2.compareTo(max) > 0) {
+				max = num2;
+			}
+			if (num3.compareTo(max) > 0) {
+				max = num3;
+			}
+			printMax(num1, num2, num3, max);
+			return max;
+		}
+
+		public static <T> void printMax(T num1, T num2, T num3, T max) {
+			System.out.println("Max is" + " " + max);
+		}
 	}
 
 }
